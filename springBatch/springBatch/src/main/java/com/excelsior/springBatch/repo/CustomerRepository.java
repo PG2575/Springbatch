0c30950f6e83717f0ca019494a60586f9311a505
Pranav Gujrathi <gujrathipranav@gmail.com>
2024-03-12 17:49:46 +0530

    Add files via upload

---
+++ springBatch/springBatch/src/main/java/com/excelsior/springBatch/repo/CustomerRepository.java
@@ -0,0 +1,10 @@
+package com.excelsior.springBatch.repo;
+
+
+import com.excelsior.springBatch.entity.Customer;
+import org.springframework.data.jpa.repository.JpaRepository;
+
+public interface CustomerRepository  extends JpaRepository<Customer,Integer> {
+
+
+}
